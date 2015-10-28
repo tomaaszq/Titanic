d924738ca04dc52c6dcbfee97f09b511e2a2705c
Tomasz Rudowicz <tomasz@rudowi.cz>
2015-10-28 22:53:34 +0100

    added categories and fixed some bugs

--- js/hanger_man.js
+++ js/hanger_man.js
@@ -1,76 +1,154 @@
-	var x = Math.floor((Math.random() * 70) + 0);
-	var tab_pass = new Array(70);
-	tab_pass[0] = "MILECZENIE OWIEC";
+	var x = Math.floor((Math.random() * 120) + 0);
+	var tab_pass = new Array(120);
+
+//  PRZYSZŁOWIA
+	tab_pass[0] = "CZAS TO PIENIĄDZ";
 	tab_pass[1] = "CO NAGLE TO PO DIABLE";
-	tab_pass[2] = "ZIELONA MILA";
-	tab_pass[3] = "ZMIERZCH";
-	tab_pass[4] = "HARRY POTTER";
-	tab_pass[5] = "BAŁWAN";
-	tab_pass[6] = "SZAMPNA";
-	tab_pass[7] = "FAJERWERKI";
-	tab_pass[8] = "HIENA";
-	tab_pass[9] = "SŁOŃ";
-	tab_pass[10] = "GEKON";
-	tab_pass[11] = "JUSTIN BIEBER";
-	tab_pass[12] = "LADY GAGA";
-	tab_pass[13] = "FRYZJER";
-	tab_pass[14] = "BALETNICA";
-	tab_pass[15] = "KIEROWCA";
-	tab_pass[16] = "NARCIARZ";
-	tab_pass[17] = "LEKARZ";
-	tab_pass[18] = "UCZEŃ";
-	tab_pass[19] = "NIEMOWLAK";
-	tab_pass[20] = "SHREK";
-	tab_pass[21] = "WĄŻ";
-	tab_pass[22] = "PINGWIN";
-	tab_pass[23] = "PIES";
-	tab_pass[24] = "KOT";
-	tab_pass[25] = "MAŁPA";
-	tab_pass[26] = "WILK";
-	tab_pass[27] = "ZAJĄC";
-	tab_pass[28] = "KRÓLIK";
-	tab_pass[29] = "PŁYWANIE";
-	tab_pass[30] = "MUMIA";
-	tab_pass[31] = "SIATKÓWKA";
-	tab_pass[32] = "SAMOLOT";
-	tab_pass[33] = "PTAK";
-	tab_pass[34] = "KARATE";
-	tab_pass[35] = "SPRZEDAWCA";
-	tab_pass[36] = "WIECZNE PIÓRO";
-	tab_pass[37] = "KSIĘGOWY";
-	tab_pass[38] = "MATRIX";
-	tab_pass[39] = "TAKSÓWKARZ";
+	tab_pass[2] = "DOBRA MINA DO ZŁEJ GRY";
+	tab_pass[3] = "SZCZĘŚLIWI CZASU NIE LICZĄ";
+	tab_pass[4] = "BEZ PRACY NIE MA KOŁACZY";
+	tab_pass[5] = "KULA U NOGI";
+	tab_pass[6] = "PRACOWITY JAK PSZCZOŁA";
+	tab_pass[7] = "CEL UŚWIĘCA ŚRODKI";
+	tab_pass[8] = "CO DWIE GŁOWY TO NIE JEDNA";
+	tab_pass[9] = "CO MA PIERNIK DO WIATRAKA";
+	tab_pass[10] = "DO WESELA SIĘ ZAGOI";
+	tab_pass[11] = "CO ZA DUŻO TO NIE ZDROWO";
+	tab_pass[12] = "GŁUPI MA SZCZĘŚCIE";
+	tab_pass[13] = "GOŚĆ W DOM BÓG W DOM";
+	tab_pass[14] = "JAKA PRACA TAKA PŁACA";
+	tab_pass[15] = "JAK CIĘ WIDZĄ TAK CIĘ PISZĄ";
+	tab_pass[16] = "JAK KUBA BOGU TAK BÓG KUBIE";
+	tab_pass[17] = "JAK TRWOGA TO DO BOGA";
+	tab_pass[18] = "JEDNA JASKÓŁKA WIOSNY NIE CZYNI";
+	tab_pass[19] = "KLĄĆ JAK SZEWC";
+	tab_pass[20] = "KŁAMSTWO MA KRÓTKIE NOGI";
+	tab_pass[21] = "KOMU W DROGĘ TEMU CZAS";
+	tab_pass[22] = "KTO PIERWSZY TEN LEPSZY";
+	tab_pass[23] = "KTO PYTA NIE BŁĄDZI";
+	tab_pass[24] = "KTO SIĘ CZUBI TEN SIĘ LUBI";
+	tab_pass[25] = "LEJE JAK Z CEBRA";
+	tab_pass[26] = "LICHO NIE ŚPI";
+	tab_pass[27] = "ŁATWO PRZYSZŁO ŁATWO POSZŁO";
+	tab_pass[28] = "MASZ BABO PLACEK";
+	tab_pass[29] = "MĄDRY POLAK PO SZKODZIE";
+	tab_pass[30] = "NADZIEJA MATKĄ GŁUPICH";
+	tab_pass[31] = "W GORĄCEJ WODZIE KĄPANY";
+	tab_pass[32] = "PŁAKAĆ NAD ROZLANYM MLEKIEM";
+	tab_pass[33] = "WYWOŁYWAĆ WILKA Z LASU";
+	tab_pass[34] = "PIERWSZE KOTY ZA PŁOTY";
+	tab_pass[35] = "RAZ NA WOZIE RAZ POD WOZEM";
+	tab_pass[36] = "STARA MIŁOŚĆ NIE RDZEWIEJE";
+	tab_pass[37] = "STRZEŻONEGO PAN BÓG STRZEŻE";
+	tab_pass[38] = "ZŁEGO DIABLI NIE BIORĄ";
+	tab_pass[39] = "WYSZŁO SZYDŁO Z WORKA";
+
+//  FILMY
+
 	tab_pass[40] = "TITANIC";
 	tab_pass[41] = "ŚWIAT WEDŁUG KIEPSKICH";
 	tab_pass[42] = "KUBUŚ PUCHATEK";
-	tab_pass[43] = "PILOT";
-	tab_pass[44] = "PIŁKA NOŻNA";
+	tab_pass[43] = "NIETYKALNI";
+	tab_pass[44] = "OJCIEC CHRZESTNY";
 	tab_pass[45] = "REQUIEM DLA SNU";
-	tab_pass[46] = "STRUŚ";
+	tab_pass[46] = "ZIELONA MILA";
 	tab_pass[47] = "BOLEK I LOLEK";
 	tab_pass[48] = "LEON ZAWODOWIEC";
 	tab_pass[49] = "SZÓSTY ZMYSŁ";
-	tab_pass[50] = "REKIN";
-	tab_pass[51] = "W GORĄCEJ WODZIE KĄPANY";
+	tab_pass[50] = "FORREST GUMP";
+	tab_pass[51] = "PULP FICTION";
 	tab_pass[52] = "INCEPCJA";
-	tab_pass[53] = "PSZCZOŁA";
-	tab_pass[54] = "WETERYNARZ";
-	tab_pass[55] = "ŚLIMAK";
-	tab_pass[56] = "NUREK";
-	tab_pass[57] = "ZEGARMISTRZ";
+	tab_pass[53] = "PODZIEMNY KRĄG";
+	tab_pass[54] = "PIANISTA";
+	tab_pass[55] = "DJANGO";
+	tab_pass[56] = "PIĘKNY UMYSŁ";
+	tab_pass[57] = "KRÓL LEW";
 	tab_pass[58] = "KEVIN SAM W DOMU";
-	tab_pass[59] = "WEHIKUŁ CZASU";
-	tab_pass[60] = "BARAN";
-	tab_pass[61] = "PRZYPADEK";
+	tab_pass[59] = "BOGOWIE";
+	tab_pass[60] = "GRA O TRON";
+	tab_pass[61] = "BREAKING BAD";
 	tab_pass[62] = "EFEKT MOTYLA";
 	tab_pass[63] = "INDIANA JONES";
 	tab_pass[64] = "WŁADCA PIERŚCIENI";
-	tab_pass[65] = "SZERMIERKA";
-	tab_pass[66] = "TOKARZ";
-	tab_pass[67] = "KOMAR";
-	tab_pass[68] = "BRUDNOPIS";
-	tab_pass[69] = "PRZYJACIELE";
-	tab_pass[70] = "PĘPEK ŚWIATA";
+	tab_pass[65] = "HOUSE OF CARDS";
+	tab_pass[66] = "W GARNITURACH";
+	tab_pass[67] = "DEXTER";
+	tab_pass[68] = "DR HOUSE";
+	tab_pass[69] = "ZAGUBIENI";
+	tab_pass[70] = "HELIKOPTER W OGNIU";
+	tab_pass[71] = "SZÓSTY ZMYSŁ";
+	tab_pass[72] = "INTERSTELLAR";
+	tab_pass[73] = "ILUZJONISTA";
+	tab_pass[74] = "SAMI SWOI";
+	tab_pass[75] = "DZIEŃ ŚWIRA";
+	tab_pass[76] = "KILER";
+	tab_pass[77] = "CHŁOPAKI NIE PŁACZĄ";
+	tab_pass[78] = "SALA SAMOBÓJCÓW";
+	tab_pass[79] = "SKAZANY NA BLUESA";
+	tab_pass[80] = "PSY";
+
+//ZESPOŁY
+	tab_pass[81] = "PERFECT";
+	tab_pass[82] = "REPUBLIKA";
+	tab_pass[83] = "KULT";
+	tab_pass[84] = "DŻEM";
+	tab_pass[85] = "BUDKA SUFLERA";
+	tab_pass[86] = "LAO CHE";
+	tab_pass[87] = "CZESŁAW NIEMEN";
+	tab_pass[88] = "MAANAM";
+	tab_pass[89] = "MAREK GRECHUTA";
+	tab_pass[90] = "COMA";
+	tab_pass[91] = "GRZEGORZ TURNAU";
+	tab_pass[92] = "PIDŻAMA PORNO";
+	tab_pass[93] = "MYSLOVITZ";
+	tab_pass[94] = "DIRE STRAITS";
+	tab_pass[95] = "LED ZEPPELIN";
+	tab_pass[96] = "ARCHIVE";
+	tab_pass[97] = "QUEEN";
+	tab_pass[98] = "THE DOORS";
+	tab_pass[99] = "PINK FLOYD";
+	tab_pass[100] = "JOHN LENNON";
+	tab_pass[101] = "METALLICA";
+	tab_pass[102] = "BLACK SABBATH";
+	tab_pass[103] = "NIRVANA";
+	tab_pass[104] = "GUNS N ROSES";
+	tab_pass[105] = "THE BEATLES";
+	tab_pass[106] = "AC DC";
+	tab_pass[107] = "DEEP PURPLE";
+	tab_pass[108] = "DEPECHE MODE";
+	tab_pass[109] = "LOUIS ARMSTRONG";
+	tab_pass[110] = "JIMI HENDRIX";
+	tab_pass[111] = "BOB DYLAN";
+	tab_pass[112] = "BRUCE SPRINGSTEEN";
+	tab_pass[113] = "SINEAD OCONNOR";
+	tab_pass[114] = "ADELE";
+	tab_pass[115] = "ERIC CLAPTON";
+	tab_pass[116] = "THE ROLLING STONES";
+	tab_pass[117] = "IRON MAIDEN";
+	tab_pass[118] = "MICHAEL JACKSON";
+	tab_pass[119] = "BOB MARLEY";
+	tab_pass[120] = "THE POLICE";
+	tab_pass[121] = "STING";
+
+// określenie kategorii
+
+
+//przysłowia
+if (x>=0&&x<40)
+{
+var category = "PRZYSŁOWIA";
+}
+//filmy
+else if (x>=40&&x<81)
+{
+var category = "KINEMATOGRAFIA";
+}
+//muzyka
+else
+{
+var category = "MUZYKA";
+}
+
 
 
 	var pass = tab_pass[x];
@@ -143,6 +221,7 @@ function start()
 
 
 	document.getElementById("alphabet").innerHTML=inside_div;
+	document.getElementById("category").innerHTML = "Wylosowałeś kategorię "+'<span style="color: yellow;">'+category+"</span>"+"!";
 
 
 	write_pass();
